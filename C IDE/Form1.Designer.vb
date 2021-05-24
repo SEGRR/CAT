476c8144aa231f471d1b8fefb3f7857f78fc068e
sufiyan <shaikhsufiyan2003@gmail.com>
2021-05-24 20:43:39 +0530

    Date - 24 May 2021 Implemnted functionality
    -open Directory
    -save File
    -Build File
    -Run File
    **Only c++ file can fun right now **
    
    now 70% work is completed

--- C IDE/Form1.Designer.vb
+++ C IDE/Form1.Designer.vb
@@ -22,26 +22,40 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.ToolBar = New System.Windows.Forms.MenuStrip()
         Me.filesTool = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenDirectory = New System.Windows.Forms.ToolStripMenuItem()
         Me.OpenFile = New System.Windows.Forms.ToolStripMenuItem()
         Me.NewFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.Save = New System.Windows.Forms.ToolStripMenuItem()
         Me.Settings = New System.Windows.Forms.ToolStripMenuItem()
         Me.SaveTool = New System.Windows.Forms.ToolStripMenuItem()
-        Me.Herracy = New System.Windows.Forms.ListBox()
+        Me.NewFIleTool = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuildTool = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RunTool = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RunPreviewBox = New System.Windows.Forms.ToolStripTextBox()
+        Me.Herracy = New System.Windows.Forms.TreeView()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
         Me.ToolBar.SuspendLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ToolBar
         '
-        Me.ToolBar.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ToolBar.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.ToolBar.ImageScalingSize = New System.Drawing.Size(20, 20)
-        Me.ToolBar.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.filesTool, Me.SaveTool})
+        Me.ToolBar.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.filesTool, Me.SaveTool, Me.NewFIleTool, Me.BuildTool, Me.RunTool, Me.RunPreviewBox})
         Me.ToolBar.Location = New System.Drawing.Point(0, 0)
         Me.ToolBar.Name = "ToolBar"
-        Me.ToolBar.Size = New System.Drawing.Size(1047, 28)
+        Me.ToolBar.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional
+        Me.ToolBar.Size = New System.Drawing.Size(1047, 31)
         Me.ToolBar.TabIndex = 0
         Me.ToolBar.Text = "MenuStrip1"
         '
@@ -49,12 +63,20 @@ Partial Class Form1
         '
         Me.filesTool.BackColor = System.Drawing.SystemColors.MenuBar
         Me.filesTool.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.filesTool.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenFile, Me.NewFile, Me.Save, Me.Settings})
+        Me.filesTool.CheckOnClick = True
+        Me.filesTool.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenDirectory, Me.OpenFile, Me.NewFile, Me.ToolStripSeparator1, Me.Save, Me.Settings})
         Me.filesTool.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.filesTool.Image = CType(resources.GetObject("filesTool.Image"), System.Drawing.Image)
         Me.filesTool.Name = "filesTool"
-        Me.filesTool.Size = New System.Drawing.Size(74, 24)
-        Me.filesTool.Text = "Files"
+        Me.filesTool.Size = New System.Drawing.Size(34, 27)
+        Me.filesTool.ToolTipText = "Files"
+        '
+        'OpenDirectory
+        '
+        Me.OpenDirectory.Image = CType(resources.GetObject("OpenDirectory.Image"), System.Drawing.Image)
+        Me.OpenDirectory.Name = "OpenDirectory"
+        Me.OpenDirectory.Size = New System.Drawing.Size(199, 26)
+        Me.OpenDirectory.Text = "Open Directory"
         '
         'OpenFile
         '
@@ -62,28 +84,33 @@ Partial Class Form1
         Me.OpenFile.Image = CType(resources.GetObject("OpenFile.Image"), System.Drawing.Image)
         Me.OpenFile.Name = "OpenFile"
         Me.OpenFile.Padding = New System.Windows.Forms.Padding(1)
-        Me.OpenFile.Size = New System.Drawing.Size(167, 26)
+        Me.OpenFile.Size = New System.Drawing.Size(201, 26)
         Me.OpenFile.Text = "Open FIles"
         '
         'NewFile
         '
         Me.NewFile.Image = CType(resources.GetObject("NewFile.Image"), System.Drawing.Image)
         Me.NewFile.Name = "NewFile"
-        Me.NewFile.Size = New System.Drawing.Size(165, 26)
+        Me.NewFile.Size = New System.Drawing.Size(199, 26)
         Me.NewFile.Text = "New File"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(196, 6)
         '
         'Save
         '
         Me.Save.Image = CType(resources.GetObject("Save.Image"), System.Drawing.Image)
         Me.Save.Name = "Save"
-        Me.Save.Size = New System.Drawing.Size(165, 26)
+        Me.Save.Size = New System.Drawing.Size(199, 26)
         Me.Save.Text = "Save"
         '
         'Settings
         '
         Me.Settings.Image = CType(resources.GetObject("Settings.Image"), System.Drawing.Image)
         Me.Settings.Name = "Settings"
-        Me.Settings.Size = New System.Drawing.Size(165, 26)
+        Me.Settings.Size = New System.Drawing.Size(199, 26)
         Me.Settings.Text = "Settings"
         '
         'SaveTool
@@ -93,24 +120,77 @@ Partial Class Form1
         Me.SaveTool.Margin = New System.Windows.Forms.Padding(5, 0, 0, 0)
         Me.SaveTool.Name = "SaveTool"
         Me.SaveTool.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
-        Me.SaveTool.Size = New System.Drawing.Size(34, 24)
+        Me.SaveTool.Size = New System.Drawing.Size(34, 27)
         Me.SaveTool.ToolTipText = "Save file (cltr+s)"
+        '
+        'NewFIleTool
+        '
+        Me.NewFIleTool.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.NewFIleTool.Image = CType(resources.GetObject("NewFIleTool.Image"), System.Drawing.Image)
+        Me.NewFIleTool.Margin = New System.Windows.Forms.Padding(5, 0, 0, 0)
+        Me.NewFIleTool.Name = "NewFIleTool"
+        Me.NewFIleTool.Size = New System.Drawing.Size(34, 27)
+        '
+        'BuildTool
+        '
+        Me.BuildTool.AutoToolTip = True
+        Me.BuildTool.BackColor = System.Drawing.Color.White
+        Me.BuildTool.ForeColor = System.Drawing.SystemColors.MenuBar
+        Me.BuildTool.Image = CType(resources.GetObject("BuildTool.Image"), System.Drawing.Image)
+        Me.BuildTool.Margin = New System.Windows.Forms.Padding(5, 0, 0, 0)
+        Me.BuildTool.Name = "BuildTool"
+        Me.BuildTool.Size = New System.Drawing.Size(34, 27)
+        Me.BuildTool.ToolTipText = "Build your file (Shift + F6)"
+        '
+        'RunTool
+        '
+        Me.RunTool.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.RunTool.Image = CType(resources.GetObject("RunTool.Image"), System.Drawing.Image)
+        Me.RunTool.Margin = New System.Windows.Forms.Padding(5, 0, 0, 0)
+        Me.RunTool.Name = "RunTool"
+        Me.RunTool.Size = New System.Drawing.Size(34, 27)
+        '
+        'RunPreviewBox
+        '
+        Me.RunPreviewBox.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.RunPreviewBox.Margin = New System.Windows.Forms.Padding(5, 0, 1, 0)
+        Me.RunPreviewBox.Name = "RunPreviewBox"
+        Me.RunPreviewBox.ReadOnly = True
+        Me.RunPreviewBox.Size = New System.Drawing.Size(150, 27)
         '
         'Herracy
         '
-        Me.Herracy.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Herracy.BackColor = System.Drawing.SystemColors.Menu
-        Me.Herracy.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Herracy.FormattingEnabled = True
-        Me.Herracy.ItemHeight = 23
-        Me.Herracy.Items.AddRange(New Object() {"hello.c", "arrays.c", "Gproject.c"})
-        Me.Herracy.Location = New System.Drawing.Point(0, 51)
+        Me.Herracy.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.Herracy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Herracy.CheckBoxes = True
+        Me.Herracy.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Herracy.LineColor = System.Drawing.Color.SandyBrown
+        Me.Herracy.Location = New System.Drawing.Point(0, 31)
         Me.Herracy.Name = "Herracy"
-        Me.Herracy.ScrollAlwaysVisible = True
-        Me.Herracy.Size = New System.Drawing.Size(192, 556)
-        Me.Herracy.TabIndex = 1
+        Me.Herracy.Size = New System.Drawing.Size(151, 584)
+        Me.Herracy.TabIndex = 2
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(151, 31)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(896, 584)
+        Me.TabControl1.TabIndex = 3
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog"
+        Me.OpenFileDialog.Title = "Open Files"
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Title = "Save File"
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
         '
         'Form1
         '
@@ -118,14 +198,17 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.ClientSize = New System.Drawing.Size(1047, 615)
+        Me.Controls.Add(Me.TabControl1)
         Me.Controls.Add(Me.Herracy)
         Me.Controls.Add(Me.ToolBar)
-        Me.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MainMenuStrip = Me.ToolBar
         Me.Name = "Form1"
-        Me.Text = "C-AT"
+        Me.Text = "CAT"
         Me.ToolBar.ResumeLayout(False)
         Me.ToolBar.PerformLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -138,5 +221,16 @@ Partial Class Form1
     Friend WithEvents Save As ToolStripMenuItem
     Friend WithEvents Settings As ToolStripMenuItem
     Friend WithEvents SaveTool As ToolStripMenuItem
-    Friend WithEvents Herracy As ListBox
+    Friend WithEvents Herracy As TreeView
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents OpenFileDialog As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents NewFIleTool As ToolStripMenuItem
+    Friend WithEvents RunPreviewBox As ToolStripTextBox
+    Friend WithEvents RunTool As ToolStripMenuItem
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents OpenDirectory As ToolStripMenuItem
+    Friend WithEvents BuildTool As ToolStripMenuItem
 End Class
--- C IDE/Form1.vb
+++ C IDE/Form1.vb
@@ -1,9 +1,294 @@
-﻿Public Class Form1
-    Private Sub TopToolStrip_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs)
+﻿
+Imports System.Collections.ObjectModel
+Imports System.IO
+Imports System.Management.Automation.Runspaces
+Imports System.Text
+Imports Microsoft.PowerShell
 
-    End Sub
+Public Class Form1
+
+    Dim OpenTabs As List(Of TabWizard) = New List(Of TabWizard)
+    Dim OpenDirectories As List(Of FileHandler) = New List(Of FileHandler)
+
+
+    Function Findfileinfo(ByVal findThisFile As String) As FileInfo
+
+        Dim a As TabWizard
+
+        If OpenTabs.Count() = 0 Then
+            ErrorProvider1.SetError(TabControl1, "No file Selected ")
+            Return Nothing
+        End If
+
+        For Each a In OpenTabs
+
+            If (a.filename.Equals(findThisFile)) Then
+                Return a.fileinfo
+            End If
+        Next
+        Return Nothing
+    End Function
+
+
 
     Private Sub TabPage1_Click(sender As Object, e As EventArgs)
 
     End Sub
+
+    Private Sub ToolBar_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles ToolBar.ItemClicked
+
+    End Sub
+
+    Private Sub TabPage1_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub SaveTool_Clicked(sender As Object, e As EventArgs) Handles SaveTool.Click
+
+        Dim selectedTab = TabControl1.SelectedTab.Text
+
+        For Each tabs As TabWizard In OpenTabs
+            If tabs.filename.Equals(selectedTab) Then
+                tabs.SaveFile()
+                Exit For
+            End If
+        Next
+
+        MessageBox.Show("File Saved Succcessfuly")
+
+
+    End Sub
+
+    Private Sub CompileProgram(sender As Object, e As EventArgs) Handles BuildTool.Click
+
+        Dim fileName As String = TabControl1.SelectedTab.Text
+        Try
+            Dim fileInformation As FileInfo = Findfileinfo(fileName)
+            Dim filePath As String = fileInformation.FullName
+            Dim command = "PowerShell -NoExit g++ -o" + filePath
+            RunPreviewBox.Text = fileName
+            Shell(command, AppWinStyle.NormalFocus)
+
+        Catch returnedNull As Exception
+
+        End Try
+
+
+
+    End Sub
+    Private Sub RunProgram(sender As Object, e As EventArgs) Handles RunTool.Click
+
+
+        Dim fileName As String = TabControl1.SelectedTab.Text
+            Dim fileInformation As FileInfo = Findfileinfo(fileName)
+            Dim filePath As String = fileInformation.DirectoryName
+            Dim filenameWithoutExtention = fileInformation.Name.Replace(".cpp", "")
+
+            Dim command = "PowerShell -NoExit cd " + fileInformation.DirectoryName + " ; g++ " + fileName + " -o " + filenameWithoutExtention + " ; " + fileInformation.DirectoryName + "\" + filenameWithoutExtention
+            RunPreviewBox.Text = fileName
+            Shell(command, AppWinStyle.NormalFocus)
+
+
+
+
+    End Sub
+    Private Sub openFile_clicked(sender As Object, e As EventArgs) Handles OpenFile.Click
+
+
+        OpenFileDialog.InitialDirectory = Directory.GetCurrentDirectory()
+        If OpenFileDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+
+
+            Try
+                '  RichTextBox1.LoadFile(OpenFileDialog.FileName, RichTextBoxStreamType.PlainText)
+            Catch fileopener As Exception
+                MessageBox.Show("error ! could not open file ")
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub ss(sender As Object, e As EventArgs) Handles Me.Closing
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+    Private Sub Open_Directory_click(sender As Object, e As EventArgs) Handles OpenDirectory.Click
+
+        FolderBrowserDialog1.Description = "open A directory"
+        FolderBrowserDialog1.ShowNewFolderButton = True
+        FolderBrowserDialog1.ShowDialog()
+
+        'ReDim OpenDirectories(OpenDirectories.Length + 1)
+
+        Dim directoryPath = FolderBrowserDialog1.SelectedPath
+
+        Dim NewDirectory As FileHandler = New FileHandler(directoryPath)
+
+        NewDirectory.CreateTreeView(Herracy)
+        Controls.Add(Herracy)
+
+        OpenDirectories.Add(NewDirectory)
+
+    End Sub
+
+    Private Sub get_file_to_tabs(sender As Object, e As EventArgs) Handles Herracy.NodeMouseDoubleClick
+
+        Dim selectedFile = Herracy.SelectedNode.Text
+        Dim Directory As FileHandler
+        Dim fileinfo As FileInfo
+
+        For Each Directory In OpenDirectories
+
+            fileinfo = Directory.findFile(selectedFile)
+
+            If fileinfo IsNot Nothing Then
+                Exit For
+            End If
+
+
+        Next
+
+
+        Dim NewTab As TabWizard
+
+        NewTab = New TabWizard(selectedFile, fileinfo)
+
+        TabControl1.TabPages.Add(NewTab.tab_page)
+        TabControl1.SelectedTab = NewTab.tab_page
+        NewTab.CreateAndLoadTextBox()
+        OpenTabs.Add(NewTab)
+
+
+
+
+    End Sub
+
+    Function runProgram(ByVal script As String) As String    ' Remove this Function V1.2
+
+        Dim runspace As Runspace
+        runspace = RunspaceFactory.CreateRunspace() ' throwing exception
+        runspace.Open()
+        Dim pipeline As Pipeline = runspace.CreatePipeline()
+        pipeline.Commands.AddScript(script)
+        pipeline.Commands.Add("Out-Script")
+
+        Dim output As Collection(Of Management.Automation.PSObject) = New Collection(Of Management.Automation.PSObject)
+        output = pipeline.Invoke()
+        runspace.Close()
+        Dim stringbuilder As StringBuilder = New StringBuilder()
+
+        For Each ps As Management.Automation.PSObject In output
+            stringbuilder.AppendLine(ps.ToString())
+        Next
+
+
+        Return stringbuilder.ToString()
+
+    End Function  'remove this Function 
+
+
+End Class
+
+
+
+
+Class TabWizard  ' this clsss will handle file opning in  tabs in tab Control and RichTextBoxes
+
+    Public tab_page As TabPage     'create new tab page to add into herracy
+    Public filename As String       'name of file opned in tabpage
+    Public textbox As RichTextBox     ' this richtextbox will go in tab_page as new control and for file editing
+    Public fileinfo As IO.FileInfo   ' information of opened file 
+    Public pageIndex As Integer        ' after adding page to Tab Control, its index will be saved here 
+    Public IsSaved As Boolean      ' to check eather file is saved or not
+    'pblic IsAllocated As Boolean
+
+    Sub New(ByVal fname As String, ByVal FileInformation As IO.FileInfo)
+        filename = fname
+        tab_page = New TabPage()          'it will genrate a new tab_page that can be then added into herracy 
+        'set tab properties 
+        tab_page.Text = filename              'set its title as file name
+        tab_page.Name = "tab_page"
+        ' tab_page.Padding.Left = 5
+
+        IsSaved = False                       'at beginning file is not saved
+        fileinfo = FileInformation
+        'IsAllocated = True
+    End Sub
+
+    Sub CreateAndLoadTextBox()       ' this sub will allocate a richtextbox and load the file into it
+        textbox = New RichTextBox()
+        tab_page.Controls.Add(textbox)
+        textbox.Dock = DockStyle.Fill
+        Try
+            textbox.LoadFile(fileinfo.FullName, RichTextBoxStreamType.PlainText)
+        Catch e As Exception
+            MessageBox.Show(" Error : File not found ")
+
+        End Try
+
+    End Sub
+
+    Sub SaveFile()
+        Try
+            textbox.SaveFile(fileinfo.FullName, RichTextBoxStreamType.PlainText)
+        Catch s As Exception
+            MessageBox.Show("Error : couldn't save file")
+        End Try
+        IsSaved = True
+    End Sub
+
+
+
+
+
+End Class
+
+Class FileHandler
+
+    Public Directory_name As String
+    Public openDirectory As IO.DirectoryInfo
+    Public subdirectory As IO.DirectoryInfo()
+    Public directoryFiles As IO.FileInfo()
+    Public Sub New(ByVal fullPath As String)
+        openDirectory = New DirectoryInfo(fullPath)
+        Directory_name = openDirectory.Name
+        subdirectory = openDirectory.GetDirectories()
+        directoryFiles = openDirectory.GetFiles()
+
+    End Sub
+    Public Sub CreateTreeView(ByRef localTree As TreeView)
+
+        localTree.Dock = DockStyle.Left
+        Dim nodecount = localTree.GetNodeCount(False)
+        localTree.CollapseAll()
+        Dim file As FileInfo
+        localTree.Nodes.Add(Directory_name)
+        For Each file In directoryFiles
+
+            localTree.Nodes(nodecount).Nodes.Add(file.Name)
+
+        Next
+
+
+
+
+    End Sub
+
+    Public Function findFile(ByVal fileName As String) As FileInfo
+
+        Dim file As FileInfo
+
+        For Each file In directoryFiles
+            If file.Name.Equals(fileName) Then
+                Return file
+            End If
+        Next
+
+        Return Nothing
+    End Function
 End Class
